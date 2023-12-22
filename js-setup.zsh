5415bc737c300d5c1667a51440c2d6832752962d
cbstodd <cbstodd@gmail.com>
2023-12-22 12:23:31 -0500

    Switched data in files and added emojies for asthetics

--- js-setup.zsh
+++ js-setup.zsh
@@ -1,89 +1,91 @@
 #!/bin/zsh
-function introMessage() {
-  echo "Today is " date
+echo 📅 "It is "`date`
 
-# First paramater "$1" is the project name, ie: "sampleProject";
-# Package manager "$2", ie: npm, pnpm, yarn, bun.
-# Second paramater "$3" is the scripting language file type, ie: "js", "ts".
-# Third paramater "$4" is the styles" file type, ie: "css", "scss", "sass", "less".
+echo "🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬🎬"
+echo -e "1️⃣). What do you want to name your application/project? ie. 'sampleApp'";
+read _project_name
 
-  
-}
+echo -e "2️⃣). What package manager do you want to use? ie. 'npm', 'pnpm', 'yarn', 'bun'?";
+read _package_manager
+
+echo -e "3️⃣). What scripting language do you want to use? ie. 'js', 'ts'?";
+read _script_language
+
+echo -e "4️⃣). What styles language do you want to use? ie. 'css', 'scss', 'sass', 'less'?";
+read _styles_language
 
 
-# First paramater "$1" is the project name, ie: "sampleProject";
-# Package manager "$2", ie: npm, pnpm, yarn, bun.
-# Second paramater "$3" is the scripting language file type, ie: "js", "ts".
-# Third paramater "$4" is the styles" file type, ie: "css", "scss", "sass", "less".
+echo "🧑🏻‍💻 Your application/project name is: "
+echo $_project_name
+echo "📦 Your package manager is: "
+echo $_package_manager
+echo "📜 Your scripting language is: "
+echo $_script_language
+echo "🎨 Your styles language is: "
+echo $_styles_language
 
-function setup() {
-  # Creates project:
-  mkdir $1 && cd $1;
-  if [[$2 $3 $4]] then
-    # Initializes the package manager:
-    # Script Type:
-    {
-      touch index.html && mkdir src && touch src/app.$3 ||
-      touch src/app.js && touch src/message.$3 ||
-      touch src/message.js
-    };
-    # Styles:
-    {
-      mkdir styles && touch styles/main.$4 ||
-      touch styles/main.css && touch styles/variables.$4 ||
-      touch styles/variables.css && touch styles/custom.$4 || touch styles/custom.css
-    };
+# 1). Create a new directory for the project:
+mkdir $_project_name && cd $_project_name;
+
+# 3). Generate files:
+{ touch index.html && mkdir src && touch src/app.$_script_language } &&
+{ mkdir styles && touch styles/main.$_styles_language };
+
+# 3). Install dependencies:
+if [[$_package_manager == "npm"]] then
+  npm init -y &&
+  npm install eslint dotenv lite-server nodemon --save-dev;
+
+  if [[$_script_language == "ts"]] then
+    npm install typescript --save-dev && tsc init;
   fi
+fi
+if [[$_package_manager == "bun" || $_package_manager == "pnpm"]] then
+  { $_package_manager init } &&
+  { $_package_manager add eslint dotenv lite-server nodemon --save-dev  };
 
-    if [[$2 == "npm"]] then
-      { npm init -y } &&
-      { npm install eslint dotenv lite-server nodemon --save-dev };
+  if [[$_script_language == "ts"]] then
+    $_package_manager add typescript --save-dev && tsc init;
+  fi
+fi
+if [[$_package_manager == "yarn"]] then
+  yarn init -y &&
+  yarn add eslint dotenv lite-server nodemon --save-dev;
 
-      if ($3 == "ts") {
-        { npm install typescript --save-dev && tsc init }
-      }
-    fi
-    if [[$2 == "bun" || $2 == "pnpm"]] then
-      { $2 init } &&
-      { $2 add eslint dotenv lite-server nodemon --save-dev };
+  if [[$_script_language == "ts"]] then
+    yarn add typescript --save-dev && tsc init;
+  fi
+fi
 
-      if ($3 == "ts") {
-        { $2 add typescript --save-dev && tsc init }
-      }
-    fi
-    if [[$2 == "yarn"]] then
-      { yarn init -y} &&
-      { yarn add eslint dotenv lite-server nodemon --save-dev };
 
-      if ($3 == "ts") {
-        { yarn add typescript --save-dev && tsc init }
-      }
-    fi
-    if ($4 == "scss") {
-      { $2 install scss --save-dev || $2 add scss --save-dev }
-    }
-    if ($4 == "sass") {
-      { $2 install sass --save-dev || $2 add sass --save-dev }
-    }
-    if ($4 == "less") {
-      { $2 less scss --save-dev || $2 add less --save-dev }
-    }
-    if ($4 == "css") {
-      { $2 install css-linter --save-dev || $2 add css-linter --save-dev }
-    }
+case $_styles_language in
+  "scss")
+if [[$_package_manager == "npm"]] then
+  $_package_manager install scss --save-dev
+else
+  $_package_manager add scss --save-dev
+fi
+;;
+"sass")
+if [[$_package_manager == "npm"]] then
+  $_package_manager install sass --save-dev
+else
+  $_package_manager add sass --save-dev
+fi
+;;
+"less")
+if [[$_package_manager == "npm"]] then
+  $_package_manager install less --save-dev
+else
+  $_package_manager add less --save-dev
+fi
+;;
+*)
+if [[$_package_manager == "npm"]] then
+  $_package_manager install css --save-dev
+else
+  $_package_manager add css --save-dev
+fi
+;;
+esac
 
-    # Creates a README.md file, Initializes Git, commits and checks-out into a new starter branch.
-    { touch README.md && echo "# $3" >> README.md };
-    { git init && gitignore .fleet .idea .vscode .vscodium } && { git checkout -b 001-setup && git add . && git commit -m "Init commit" };
-
-  }
-  if ($2 || $3 || $4 == "undefined") {
-    echo "⚠️  Error: You are missing a paramater. ⚠️ "
-    echo "
-    1st). First paramater :'$1' is the project name, ie: 'sampleProject'
-    2nd). Package manager '$2', ie: npm, pnpm, yarn, bun.
-    3rd). Second paramater '$3' is the scripting language file type, ie: 'js', 'ts'.
-    4th). Third paramater '$4' is the styles file type, ie: 'css', 'scss', 'sass', 'less'.
-    "
-  }
-}
